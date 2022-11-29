bd96fa95207568c2021a94ff01780b8fd4ff6dec
Luca-Ferri <lucaferrip@gmail.com>
2022-11-29 15:00:56 -0300

    Adicionando logo.svg na pasta assets. Também foi criado o componente Cardapio

---
+++ src/pages/Cardapio/index.tsx
@@ -0,0 +1,14 @@
+import { ReactComponent as Logo } from 'assets/logo.svg';
+import { Menu } from './components';
+
+const Cardapio = () => {
+    return (
+        <main>
+            <Menu>
+                <Logo/>
+            </Menu>
+        </main>
+    );
+}
+ 
+export default Cardapio;
